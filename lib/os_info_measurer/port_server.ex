dfa689c4a648011d0188d7039df0634f8d996fdf
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-03-26 15:53:32 +0900

    Update sample logic of PortServer

--- lib/os_info_measurer/port_server.ex
+++ lib/os_info_measurer/port_server.ex
@@ -7,6 +7,10 @@ defmodule OsInfoMeasurer.PortServer do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
+  def start_measure() do
+    GenServer.call(__MODULE__, :start_measure)
+  end
+
   def init(_args) do
     port = Port.open({:spawn, "./a.out"}, [:binary])
     {:ok, %{port: port}}
@@ -15,5 +19,10 @@ defmodule OsInfoMeasurer.PortServer do
   def handle_info({_port, {:data, data}}, state) do
     Logger.info("#{inspect(data)}")
     {:noreply, state}
+  end
+
+  def handle_call(:start_measure, _from, state) do
+    Port.command(state.port, "start\n")
+    {:reply, :ok, state}
   end
 end
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "hello" << std::endl;
+    std::string command;
+
+    while (true)
+    {
+        std::cin >> command;
+
+        if (std::cin.eof())
+        {
+            // NOTE: Erlang VM のシャットダウンで標準入力が閉じられると EOF となる
+            //       それをトリガーに終了させる
+            std::cerr << "eof" << std::endl;
+            break;
+        }
+
+        if (std::cin.fail())
+        {
+            std::cerr << "fail" << std::endl;
+            break;
+        }
+
+        std::cout << command << std::endl;
+    }
     return 0;
 }
