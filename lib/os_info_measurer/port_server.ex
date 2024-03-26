a106f5c58c80744429e08cc9ee8add2447aacc25
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-03-26 11:54:21 +0900

    Add sample logic to PortServer

--- lib/os_info_measurer/port_server.ex
+++ lib/os_info_measurer/port_server.ex
@@ -1,11 +1,19 @@
 defmodule OsInfoMeasurer.PortServer do
   use GenServer
 
+  require Logger
+
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   def init(_args) do
-    {:ok, %{}}
+    port = Port.open({:spawn, "./a.out"}, [:binary])
+    {:ok, %{port: port}}
+  end
+
+  def handle_info({_port, {:data, data}}, state) do
+    Logger.info("#{inspect(data)}")
+    {:noreply, state}
   end
 end
