d11cfe959dc14d926b15e545e8efb9ac5b48ac06
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-03-26 11:48:40 +0900

    Add :observer

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule OsInfoMeasurer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools, :wx, :observer],
       mod: {OsInfoMeasurer.Application, []}
     ]
   end
