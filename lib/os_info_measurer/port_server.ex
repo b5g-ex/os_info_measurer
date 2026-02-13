b43a4ce9326e7c60453067c34e97085e238485f0
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-02-13 13:40:14 +0900

    Update port_server.ex

--- lib/os_info_measurer/port_server.ex
+++ lib/os_info_measurer/port_server.ex
@@ -3,8 +3,12 @@ defmodule OsInfoMeasurer.PortServer do
 
   require Logger
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  def open(data_directory_path, file_name_prefix, interval_ms) do
+    GenServer.call(__MODULE__, {:open, data_directory_path, file_name_prefix, interval_ms})
+  end
+
+  def close() do
+    GenServer.call(__MODULE__, :close)
   end
 
   def start_measuring() do
@@ -15,23 +19,17 @@ defmodule OsInfoMeasurer.PortServer do
     GenServer.call(__MODULE__, :stop_measure)
   end
 
-  def open(data_directory_path, file_name_prefix, interval_ms) do
-    GenServer.call(__MODULE__, {:open, data_directory_path, file_name_prefix, interval_ms})
-  end
-
-  def close() do
-    GenServer.call(__MODULE__, :close)
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   def init(_args) do
     Process.flag(:trap_exit, true)
 
-    bin_path =
-      Application.app_dir(:os_info_measurer)
-      |> Path.join("priv/measurer")
+    bin_path = Application.app_dir(:os_info_measurer) |> Path.join("priv/measurer")
 
     if File.exists?(bin_path) do
-      {:ok, %{port: nil, bin_path: bin_path}}
+      {:ok, %{port: nil, bin_path: bin_path, measuring: false}}
     else
       {:stop, "#{bin_path} not found"}
     end
@@ -46,42 +44,68 @@ defmodule OsInfoMeasurer.PortServer do
     {:noreply, state}
   end
 
-  def handle_call(:start_measure, _from, state) do
-    Port.command(state.port, "start\n")
-    {:reply, :ok, state}
-  end
-
-  def handle_call(:stop_measure, _from, state) do
-    Port.command(state.port, "stop\n")
-    {:reply, :ok, state}
-  end
-
   def handle_call({:open, data_directory_path, file_name_prefix, interval_ms}, _from, state) do
-    if is_nil(state.port) do
-      port =
-        Port.open(
-          {:spawn,
-           "#{state.bin_path} -d #{data_directory_path} -f #{file_name_prefix} -i #{interval_ms}"},
-          [
-            :binary,
-            :exit_status
-          ]
-        )
+    cond do
+      not is_nil(state.port) ->
+        {:reply, {:error, :already_opened}, state}
 
-      {:reply, :ok, %{state | port: port}}
-    else
-      Logger.error("already opened")
-      {:reply, :error, state}
+      state.measuring ->
+        {:reply, {:error, :measuring_in_progress}, state}
+
+      true ->
+        port =
+          Port.open(
+            {:spawn,
+             "#{state.bin_path} -d #{data_directory_path} -f #{file_name_prefix} -i #{interval_ms}"},
+            [
+              :binary,
+              :exit_status
+            ]
+          )
+
+        {:reply, :ok, %{state | port: port}}
     end
   end
 
   def handle_call(:close, _from, state) do
-    if is_nil(state.port) do
-      Logger.error("already closed")
-      {:reply, :error, state}
-    else
-      Port.close(state.port)
-      {:reply, :ok, %{state | port: nil}}
+    cond do
+      is_nil(state.port) ->
+        {:reply, {:error, :already_closed}, state}
+
+      state.measuring ->
+        {:reply, {:error, :measuring_in_progress}, state}
+
+      true ->
+        Port.close(state.port)
+        {:reply, :ok, %{state | port: nil}}
+    end
+  end
+
+  def handle_call(:start_measure, _from, state) do
+    cond do
+      is_nil(state.port) ->
+        {:reply, {:error, :not_opened}, state}
+
+      state.measuring ->
+        {:reply, {:error, :already_measuring}, state}
+
+      true ->
+        Port.command(state.port, "start\n")
+        {:reply, :ok, %{state | measuring: true}}
+    end
+  end
+
+  def handle_call(:stop_measure, _from, state) do
+    cond do
+      is_nil(state.port) ->
+        {:reply, {:error, :not_opened}, state}
+
+      not state.measuring ->
+        {:reply, {:error, :not_measuring}, state}
+
+      true ->
+        Port.command(state.port, "stop\n")
+        {:reply, :ok, %{state | measuring: false}}
     end
   end
 end
