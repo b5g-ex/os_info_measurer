9797944bfbf5928cb2860df39180d3e32b2145ab
Ryota Kinukawa <kinukawa@tombo-works.com>
2026-02-16 14:40:46 +0900

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OsInfoMeasurer.MixProject do
   def project do
     [
       app: :os_info_measurer,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
